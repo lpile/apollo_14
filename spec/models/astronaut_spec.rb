5be9ea9d68939a1a977572767a4b70ca7f37e1f7
lpile <logan.pile@gmail.com>
2019-05-10 10:53:28 -0600

    Add #average_age spec

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 describe Astronaut, type: :model do
+
+  before :each do
+    @astronaut_1 = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+    @astronaut_2 = Astronaut.create!(name: 'Buzz Aldrin', age: 38, job: 'Engineer')
+    @astronaut_3 = Astronaut.create!(name: 'Jeff Book', age: 42, job: 'Custodian')
+    @mission_1 = @astronaut_1.missions.create!(title: "Capricorn 4", time_in_space: 55)
+    @mission_2 = @astronaut_1.missions.create!(title: "Apollo 13", time_in_space: 22)
+    @mission_3 = @astronaut_1.missions.create!(title: "Gemini 7", time_in_space: 3)
+    @mission_4 = @astronaut_2.missions.create!(title: "Apollo 7", time_in_space: 34)
+    @mission_5 = @astronaut_2.missions.create!(title: "Taurus 7", time_in_space: 32)
+  end
+
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
@@ -10,5 +22,13 @@ describe Astronaut, type: :model do
   describe 'Relationships' do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
+  end
+
+  describe 'Class Methods' do
+    it ".average_age" do
+      average = (37 + 38 + 42) / 3.0
+
+      expect(Astronaut.average_age.round(2)).to eq(average.round(2))
+    end
   end
 end
