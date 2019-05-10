3cbae9a8636f343effcc33fa3ab8f83ad234e3b5
lpile <logan.pile@gmail.com>
2019-05-10 09:59:26 -0600

    Add User Story 3 spec

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -26,5 +26,19 @@ RSpec.describe "As a visitor", type: :feature do
       expect(Astronaut.average_age).to eq(34)
       expect(page).to have_content("Average Age: 34")
     end
+
+    it "I see a list of the space missions" do
+      astronaut_1 = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+      astronaut_2 = Astronaut.create!(name: 'Bob Armstrong', age: 31, job: 'Commander')
+      mission_1 = astronaut_1.missions.create!(title: "Capricorn 4", time_in_space: 55)
+      mission_2 = astronaut_1.missions.create!(title: "Apollo 13", time_in_space: 22)
+      mission_3 = astronaut_1.missions.create!(title: "Gemini 7", time_in_space: 3)
+
+      visit '/astronauts'
+
+      expect(page).to have_content(mission_1)
+      expect(page).to have_content(mission_2)
+      expect(page).to have_content(mission_3)
+    end
   end
 end
