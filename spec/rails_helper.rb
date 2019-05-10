057111fdda514611af3a56936c5e04065f6ebdbd
lpile <logan.pile@gmail.com>
2019-05-10 11:33:19 -0600

    Add asserting sort order

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,12 @@ require 'rspec/rails'
 require 'simplecov'
 SimpleCov.start
 
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    page.body.index(earlier_content) < page.body.index(later_content)
+  end
+end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
