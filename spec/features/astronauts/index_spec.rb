bb77fb7231e36abe4526ff09d77d8c27883f02c6
lpile <logan.pile@gmail.com>
2019-05-10 09:51:37 -0600

    Add User Story 2 spec

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -10,11 +10,21 @@ RSpec.describe "As a visitor", type: :feature do
     it "I see a list of astronauts info" do
       astronaut = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
 
-      visit astronauts_path
+      visit '/astronauts'
 
       expect(page).to have_content(astronaut.name)
       expect(page).to have_content(astronaut.age)
       expect(page).to have_content(astronaut.job)
+    end
+
+    it "I see the average age of all astronauts" do
+      astronaut_1 = Astronaut.create!(name: 'Neil Armstrong', age: 37, job: 'Commander')
+      astronaut_2 = Astronaut.create!(name: 'Bob Armstrong', age: 31, job: 'Commander')
+
+      visit '/astronauts'
+
+      expect(Astronaut.average_age).to eq(34)
+      expect(page).to have_content("Average Age: 34")
     end
   end
 end
